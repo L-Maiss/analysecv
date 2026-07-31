4b961c32fe0f36dfc3210007f87e95c323919905
L-Maiss <luka.maisuradze.code@gmail.com>
2026-08-01 00:15:38 +0400

    changed logo

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -2,11 +2,7 @@
 
     <div class="flex items-center justify-between p-4 max-w-7xl mx-auto">
         <a href="{{ route('home') }}" class="flex items-center justify-between gap-4">
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-8 w-auto">
-
-            <h1 class="text-xl font-bold">CareerAI</h1>
-
-            <h4 class="text-xs">AI Powered</h4>
+            <img src="{{ asset('images/analysecv_logo_bg1.png') }}" alt="Logo" class="h-12 w-auto">
         </a>
 
         <div class="flex items-center justify-between gap-4">
